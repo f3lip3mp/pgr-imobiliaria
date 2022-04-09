69c108952ccb90f162dfcd86bd73430476253620
f3lip3mp <pmenezesfelipe@gmail.com>
2022-04-09 00:01:46 -0300

    :sparkles: Adiciona a função para ler o arquivo imobiliarias.txt

--- base.py
+++ base.py
@@ -7,3 +7,11 @@ from os.path import join
  
 arquivo_imobiliaria = join('imobiliarias.txt')
 
+def ler_arquivo_imobiliaria():
+    lista_imobiliarias = []
+    with open(arquivo_imobiliaria, 'r') as imobiliarias:
+        for x in imobiliarias.readlines():
+            # Separa o elementos pelo separador ; e remove espaços
+            lista_imobiliarias.append(x.strip().split(';'))
+        imobiliarias.close()
+    return lista_imobiliarias
